7a8cb306d5273d30874d64feabecbe5f96ea95b8
Marcus Leong <162563608+marcussleongg@users.noreply.github.com>
2025-08-31 22:19:06 +0800

    Remove borders for loading state for mobile

--- src/app/blog/[slug]/loading.tsx
+++ src/app/blog/[slug]/loading.tsx
@@ -8,7 +8,7 @@ export default function Loading() {
       </div>
 
       <div className="w-full border-b border-gray-200 my-4 [@media(hover:hover)]:hidden"></div>
-      <div className="space-y-6 mt-8 flex-grow max-h-[70vh] overflow-y-auto border-3 border-dotted p-6 rounded-lg max-md:w-full md:min-w-2xl border-[hsl(331,13%,54%)] flex justify-center items-center">
+      <div className="space-y-6 mt-8 flex-grow max-h-[70vh] overflow-y-auto [@media(hover:hover)]:border-3 border-dotted p-6 rounded-lg max-md:w-full md:min-w-2xl border-[hsl(331,13%,54%)] flex justify-center items-center">
         <div className="flex space-x-1">
             <div className="w-2 h-2 bg-gray-600 dark:bg-gray-400 rounded-full"></div>
             <div className="w-2 h-2 bg-gray-600 dark:bg-gray-400 rounded-full animate-pulse" style={{ animationDelay: '0.2s' }}></div>
