68d1d0156d5352eddafed89e52c2bf1f6f518456
Marcus Leong <162563608+marcussleongg@users.noreply.github.com>
2025-09-01 22:50:47 +0800

    Fix hovering on mobile and layout settings

--- src/components/USAmap.tsx
+++ src/components/USAmap.tsx
@@ -10,13 +10,11 @@ interface USAMapComponentProps {
 
 const USAMapComponent = ({ statesData }: USAMapComponentProps) => {
   const [selectedState, setSelectedState] = useState<USAStateAbbreviation | null>(null);
-  
+
   const stateContent = statesData.reduce((acc, stateData) => {
     acc[stateData.state] = stateData;
     return acc;
   }, {} as Record<USAStateAbbreviation, StateData>);
-
-  console.log(stateContent);
   
   const visitedStates = statesData.map(state => state.state);
 
@@ -34,14 +32,16 @@ const USAMapComponent = ({ statesData }: USAMapComponentProps) => {
   const selectedStateData = selectedState ? stateContent[selectedState] : null;
 
   return (
-    <div className="flex flex-col [@media(hover:hover)]:flex-row w-full gap-4 [@media(hover:hover)]:gap-8">
-      <div className="[@media(hover:hover)]:w-2/3 xl:w-auto xl:flex-shrink-0">
+    <div className="flex flex-col [@media(hover:hover)_and_(min-width:1080px)]:flex-row w-full gap-4 [@media(hover:hover)_and_(min-width:1080px)]:gap-8">
+      <div className="[@media(hover:hover)_and_(min-width:1080px)]:flex-shrink-0">
         <p className="mb-8">My goal is to visit <strong>all 50 states</strong> before I graduate (hopeful I know). Click on visited states to see my experience!</p>
         <div className="w-full max-w-2xl [@media(hover:none)]:max-w-full">
-          <USAMap customStates={customStates} />
+          <USAMap 
+            customStates={customStates}
+          />
         </div>
       </div>
-      <div className="[@media(hover:hover)]:w-1/3 xl:w-auto xl:flex-1">
+      <div className="[@media(hover:hover)_and_(min-width:1080px)]:flex-1">
         {selectedStateData ? (
           <div>
             <h2 className="text-2xl font-semibold mb-4">
